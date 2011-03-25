f29770a198f6a7954a8e5d12c9987023f0b69adf
mtomczak <iam@fixermark.com>
2011-03-25 08:22:41 -0400

    Tracking servant model.

---
+++ app/models/servant.rb
@@ -0,0 +1,23 @@
+require 'json'
+
+class Servant < ActiveRecord::Base
+  validates_uniqueness_of :name
+  def to_json(*a)
+    {
+      'name' => self.name,
+      'url' => self.url,
+      'protocol' => self.protocol
+    }.to_json(*a)
+  end
+  def from_json(json)
+    json_hash = JSON.parse(json)
+    self.name = json_hash['name']
+    self.url = json_hash['url']
+    self.protocol = json_hash['protocol']
+  end
+  def self.new_from_json(json)
+    result = self.new
+    result.from_json(json)
+    result
+  end
+end
