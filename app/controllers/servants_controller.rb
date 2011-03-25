374df63ca5ebcf1a0a5bc823a2c7afc5cd2a64b6
mtomczak <iam@fixermark.com>
2011-03-25 01:04:49 -0400

    Added communicate.js, which allows for smoother AJAX communication.

---
+++ app/controllers/servants_controller.rb
@@ -0,0 +1,22 @@
+class ServantsController < ApplicationController
+  def add_servant
+    new_servant = Servant.new_from_json(request.raw_post)
+    if not new_servant.save
+      respond_app_error ("DuplicateRecord")
+    else
+      respond_ok
+    end
+  end
+
+  def get_servants
+    @servants = Servant.find(:all)
+    
+  end
+
+  def update_servant
+  end
+
+  def delete_servant
+  end
+
+end
