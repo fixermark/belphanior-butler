3e0ec383185490eb14ecce50fb688044982e97a8
mtomczak <iam@fixermark.com>
2011-03-25 08:21:40 -0400

    Cleaned up communicate.js and added attempt to retrieve data from server.

--- public/javascripts/communicate.js
+++ public/javascripts/communicate.js
@@ -18,78 +18,79 @@ function AjaxError(error_class, short_description, long_description) {
 function Ajax(default_success_callback, default_fail_callback) {
   this.default_success_callback = default_success_callback;
   this.default_fail_callback = default_fail_callback;
-}
-
-Ajax.prototype.do(method, uri, data, success_callback, fail_callback) {
-  $.ajax({
-    type: method,
-    url: uri,
-    dataType: "json",
-    success: this.success_handler(
-      success_callback,
-      fail_callback),
-    error: this.error_handler(fail_callback)
-  });      
-}
-
-Ajax.prototype.get(uri, success_callback, fail_callback) {
-  this.do("GET", uri, null, success_callback, fail_callback);
-}
-
-Ajax.prototype.post(uri, data, success_callback, fail_callback) {
-  this.do("POST", uri, data, success_callback, fail_callback);
-}
-
-Ajax.prototype.success_handler(success_callback, fail_callback) {
-  var ajax_object = this;
-  return function(data, textStatus, jqXHR) {
-    if (jqXHR.status != 200) {
-      ajax_object.invoke_fail_callback(
-        fail_callback,
-        new AjaxError(
-          "StatusError",
-          "Response code was " + jqXHR.status + " instead of 200.",
-          "Response code was " + jqXHR.status + " instead of 200.<br>" +
-             "Document body:<br>" +
-             jqXHR.responseText));
-    } else {
-      ajax_object.invoke_success_callback(success_callback, data);
-    }
-  }  // end success handler
-}
-
-Ajax.prototype.error_handler(fail_callback) {
-  var ajax_object = this;
-  return function (jqXHR, textStatus, errorThrown) {
-    if (jqXHR.status != 200) {
-      ajax_object.invoke_fail_callback(
-        fail_callback,
-        new AjaxError(
-          "StatusError",
-          "Response code was " + jqXHR.status + " instead of 200.",
-          "Response code was " + jqXHR.status + " instead of 200.<br>" +
-             "Document body:<br>" +
-             jqXHR.responseText));
-    } else {
-      ajax_object.invoke_fail_callback(
-        fail_callback,
-        new AjaxError(textStatus, textStatus, textStatus);
-    }
-  }  // end error handler
-}
-
-Ajax.prototype.invoke_success_callback(success_callback, deserialized_object) {
-  if (success_callback) {
-    success_callback(deserialized_object);
-  } else {
-    this.default_success_callback(deserialized_object);
+  this.communicate = function(
+    method, uri, data, success_callback, fail_callback) {
+    $.ajax({
+      type: method,
+      url: uri,
+      dataType: "json",
+      success: this.success_handler(
+        success_callback,
+        fail_callback),
+      error: this.error_handler(fail_callback)
+    });      
   }
-}
 
-Ajax.prototype.invoke_fail_callback(fail_callback, error_data) {
-  if (fail_callback) {
-    fail_callback(error_data);
-  } else {
-    this.default_fail_callback(error_data);
+  this.get = function (uri, success_callback, fail_callback) {
+    this.communicate("GET", uri, null, success_callback, fail_callback);
+  }
+
+  this.post = function (uri, data, success_callback, fail_callback) {
+    this.communicate("POST", uri, data, success_callback, fail_callback);
+  }
+
+  this.success_handler = function(success_callback, fail_callback) {
+    var ajax_object = this;
+    return function(data, textStatus, jqXHR) {
+      if (jqXHR.status != 200) {
+        ajax_object.invoke_fail_callback(
+          fail_callback,
+          new AjaxError(
+            "StatusError",
+            "Response code was " + jqXHR.status + " instead of 200.",
+            "Response code was " + jqXHR.status + " instead of 200.<br>" +
+               "Document body:<br>" +
+               jqXHR.responseText));
+      } else {
+        ajax_object.invoke_success_callback(success_callback, data);
+      }
+    }  // end success handler
+  }
+
+  this.error_handler = function(fail_callback) {
+    var ajax_object = this;
+    return function (jqXHR, textStatus, errorThrown) {
+      if (jqXHR.status != 200) {
+        ajax_object.invoke_fail_callback(
+          fail_callback,
+          new AjaxError(
+            "StatusError",
+            "Response code was " + jqXHR.status + " instead of 200.",
+            "Response code was " + jqXHR.status + " instead of 200.<br>" +
+               "Document body:<br>" +
+               jqXHR.responseText));
+      } else {
+        ajax_object.invoke_fail_callback(
+          fail_callback,
+          new AjaxError(textStatus, textStatus, textStatus));
+      }
+    }  // end error handler
+  }
+
+  this.invoke_success_callback = function(
+    success_callback, deserialized_object) {
+    if (success_callback) {
+      success_callback(deserialized_object);
+    } else {
+      this.default_success_callback(deserialized_object);
+    }
+  }
+
+  this.invoke_fail_callback = function(fail_callback, error_data) {
+    if (fail_callback) {
+      fail_callback(error_data);
+    } else {
+      this.default_fail_callback(error_data);
+    }
   }
 }
