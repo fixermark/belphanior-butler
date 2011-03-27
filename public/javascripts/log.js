59f55fa44d8b43dc15e1af0e858dd288c92ab7f8
mtomczak <iam@fixermark.com>
2011-03-26 20:32:48 -0400

    Added logging facility (and corresponding UI element)

---
+++ public/javascripts/log.js
@@ -0,0 +1,16 @@
+// Generalized logging engine
+
+function Logger() {}
+
+Logger.show_log = function() {
+  $("#log").show();
+}
+
+Logger.hide_log = function() {
+  $("#log").hide();
+}
+
+Logger.log = function(data) {
+  $("#log_content").html(
+    $("#log_content").html() + "<li>" + data + "</li>");
+}
