4d109a8e7b4b3c98c5341b516e776f6e2f47a810
mtomczak <iam@fixermark.com>
2011-07-04 11:11:38 -0400

    Functional test for documentation controller.

--- app/controllers/documentation_controller.rb
+++ app/controllers/documentation_controller.rb
@@ -1,2 +1,17 @@
+require 'json'
+
 class DocumentationController < ApplicationController
+  def index
+    # Retrieves the documentation for all of the known
+    # Roles.
+    # See the Belphanior documentation for more information
+    # on the formatting for a Role description.
+    roles_from_db = Role.find(:all, :order=>"name ASC")
+    @roles = []
+    roles_from_db.each do |role|
+      if role.model?
+        @roles << role.model
+      end
+    end
+  end
 end
--- test/functional/documentation_controller_test.rb
+++ test/functional/documentation_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class DocumentationControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Getting documentation" do
+    get :index
+    roles = assigns(:roles)
+    assert_equal(roles.length, 2)
+    role_data = JSON.parse(roles[0])
+    assert_equal(role_data["name"], "alpha")
+    assert_equal(role_data["data"], "alpha data")
+
+    role_data = JSON.parse(roles[1])
+    assert_equal(role_data["name"], "charlie")
+    assert_equal(role_data["data"], "charlie data")
   end
 end
