c7ae1fc836913db6d4763f70aee42f2168b4532e
mtomczak <iam@fixermark.com>
2011-07-16 14:44:49 -0400

    Created immediate-mode path for argument resolution.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -10,7 +10,11 @@ class ScriptRunner
   end
   def initialize
     @servants_by_name={}
-    # TODO(mtomczak): Init runners for all servants
+  end
+
+  def anonymous_script
+    # Provide a context in which to run an anonymous script
+    binding
   end
   
   def servant_by_name(name)
@@ -24,20 +28,22 @@ class ScriptRunner
     servant_name = params[:servant]
     servant_role = params[:role]
     
-    if not @servants_by_name.has_key? servant_name then
-      raise TellFailure, "No servant by name #servant_name."
+    servant = servant_by_name(servant_name)
+    if not servant then
+      raise ServantLoadFailure, "Servant with name '#{servant_name}' was nil."
     end
     
-    yield @servants_by_name[servant_name].get_context_by_name(servant_role)
+    yield servant.get_context_by_name(servant_role)
   end
+
  private
   def cache_servant_caller(name)
     servant = Servant.find_by_name(name)
     if not servant then
-      raise ServantLoadFailure, "No servant with name #name found."
+      raise ServantLoadFailure, "No servant with name '#{name}' found."
     end
     if servant.status != :loaded then
-      raise ServantLoadFailure, "Servant #name is not loaded."
+      raise ServantLoadFailure, "Servant '#{name}' is not loaded."
     end
     # TODO(mtomczak): We have a working servant; cache it.
     roles_by_url = {}
@@ -48,8 +54,9 @@ class ScriptRunner
         roles_by_url[url.to_s]=JSON.parse(role.model)
       end
     end
-    servant_caller = ServantCaller.new(
-      logger, 
+    
+    servant_caller = Belphanior::ServantCaller.new(
+      RAILS_DEFAULT_LOGGER, 
       servant.url.to_s,
       JSON.parse(servant.protocol),
       roles_by_url)
