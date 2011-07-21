42ef085b7be0b56e2384300f573c4f6bf8545101
mtomczak <iam@fixermark.com>
2011-07-20 21:16:53 -0400

    Added fields to command_button migrate method.

--- db/migrate/20110721005521_create_command_buttons.rb
+++ db/migrate/20110721005521_create_command_buttons.rb
@@ -1,7 +1,10 @@
 class CreateCommandButtons < ActiveRecord::Migration
   def self.up
     create_table :command_buttons do |t|
-
+      # Name of the button.
+      t.string :name
+      # Command executed when the button is pushed.
+      t.text :command
       t.timestamps
     end
   end
