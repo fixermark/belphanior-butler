6efa24bedd143d6cf8d1a8d3e1234f242cc272a0
mtomczak <iam@fixermark.com>
2011-07-20 21:10:58 -0400

    Added json serializers to button object.

--- app/models/command_button.rb
+++ app/models/command_button.rb
@@ -1,2 +1,15 @@
+require 'json'
+
 class CommandButton < ActiveRecord::Base
+  def from_json(json)
+    json_hash = JSON.parse(json)
+    self.name = json_hash['name']
+    self.command = json_hash['command']
+  end
+
+  def self.new_from_json(json)
+    result = self.new
+    result.from_json(json)
+    result
+  end
 end
