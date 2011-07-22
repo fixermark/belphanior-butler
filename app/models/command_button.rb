07ef2540e1de2e28aabc8c27e089204b1ed2e769
mtomczak <iam@fixermark.com>
2011-07-22 07:25:47 -0400

    Added to_json method for command buttons.

--- app/models/command_button.rb
+++ app/models/command_button.rb
@@ -7,6 +7,14 @@ class CommandButton < ActiveRecord::Base
     self.command = json_hash['command']
   end
 
+  def to_json(*a)
+    {
+      'id' => self.id,
+      'name' => self.name,
+      'command' => self.command
+    }.to_json(*a)
+  end
+
   def self.new_from_json(json)
     result = self.new
     result.from_json(json)
