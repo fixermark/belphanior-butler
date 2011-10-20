254a058ca039780a0d5133af9d2c789e9c4e4a62
mtomczak <iam@fixermark.com>
2011-10-20 07:56:28 -0400

    Added initializers.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BelphaniorButlerNewTemplate::Application.config.secret_token = '688e381976c68970941d3b95f230b168918e5868ea996df5d955103f418b4526e1c5abb070aae2d7efbf1bc588beaf1be60466f4fc26f0521d44780b4315b545'
