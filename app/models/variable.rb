8063f6224ae0993ba3ed04fb07e1b7ac4ade7768
Mark Tomczak <iam@fixermark.com>
2011-12-28 21:50:09 -0500

    Test for variables.

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,2 +1,14 @@
 class Variable < ActiveRecord::Base
+  validates_uniqueness_of :name
+  def as_json(*a)
+    {
+      'name' => self.name,
+      'value' => self.value
+    }
+  end
+  def self.new_from_json(json)
+    result = self.new
+    result.from_json(json)
+    result
+  end
 end
