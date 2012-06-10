b4364dfe111f12b3185b7d91078834a39ffaf757
mtomczak <iam@fixermark.com>
2012-06-10 14:19:50 -0400

    Script remote triggering works.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -140,4 +140,32 @@ class ApplicationController < ActionController::Base
     end
     render :status => 500, :json => response
   end
+  def run_script_text(command_text)
+    runner = ScriptRunner.new
+    begin
+      respond_with_json(
+        :response => eval(
+          command_text,
+          runner.anonymous_script,
+          "<Immediate script>",
+          1))
+    rescue Exception => e
+      response = "Evaluation failed: An error occurred.\n"
+      response << e.to_str() << "\n"
+      seen_immediate_script = false
+      e.backtrace.each do |err_line|
+        if err_line.start_with? "<Immediate script>" then
+          seen_immediate_script = true
+        else
+          if seen_immediate_script then
+            break
+          end
+        end
+        response << err_line + "\n"
+      end
+      respond_with_json(:response => response)
+    end
+
+    # Runs a Rails script
+  end
 end
