81a4c02a01f872264a36f7aaedbd4bcc5ef2eae6
Mark T. Tomczak <iam@fixermark.com>
2012-09-04 00:58:29 -0400

    Added unary op file.

---
+++ app/controllers/blockly/code/unary_op.rb
@@ -0,0 +1,40 @@
+# Unary operator (essentially, a builtin function).
+require 'blockly/code/root'
+
+module Blockly
+  module Code
+    class UnaryOp < Root
+      def initialize(block_id, operator, argument)
+        super(block_id)
+        @operator = operator.to_sym
+        @argument = argument
+      end
+      def evaluate(context)
+        value = @argument.evaluate(context)
+        case @operator
+        when :ROOT
+          Math.sqrt value
+        when :ABS
+          value.abs
+        when :NEG
+          -(value)
+        when :LN
+          Math.log(value)
+        when :LOG10
+          Math.log10(value)
+        when :EXP
+          Math.exp(value)
+        when :POW10
+          10 ** value
+        # Rounding
+        when :ROUND
+          value.round
+        when :ROUNDUP
+          value.ceil
+        when :ROUNDDOWN
+          value.floor
+        end
+      end
+    end
+  end
+end
