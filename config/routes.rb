4c2868a77e43c47540015c69763c9fd5dcd0cf90
Mark T. Tomczak <iam@fixermark.com>
2012-11-04 21:47:08 -0500

    Re-routed default to control panel, not some place silly.

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ BelphaniorButlerNewTemplate::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :controller => 'control_panel', :action => '/'
 
   # See how all your routes lay out with "rake routes"
 
